892e1110f1a890cb7d0263ffa1562cfe01a8d35a
oliveirazpm11-jpg <oliveirazpm11@gmail.com>
2026-05-09 11:58:47 -0300

    Calculo do IMC

---
+++ 03_python_entradas/IMC.py
@@ -0,0 +1,9 @@
+#Autor:Pietro Oliveira
+
+#variaveis
+nome= input('me diga seu nome: ')
+peso=float(input('me diga seu peso: '))
+altura=float(input('me diga sua altura'))
+imc= peso/(altura**2)
+print(f"Seu imc é:{imc}")
+
---
+++ 03_python_entradas/entradas5.py
@@ -0,0 +1,14 @@
+#Autor:Pietro Oliveira
+
+#variaveis
+nome=(input('digite seu nome: '))
+valor1= int(input('digite o primeiro valor: '))
+valor2=int(input('digite o segundo valor: '))
+soma= valor1+valor2
+subtracao= valor1-valor2
+multiplicacao= valor1*valor2
+divisao=valor1/valor2
+print(f'resultado da soma é: {soma}')
+print(f'resultado da subtração é: '{soma})
+print(f'o resultado da multiplicação é: '{multiplicacao})
+print(f'o resultado da divisão é: ',{divisao})
