1e03912c86e9bb020ad1c5b6493afdbbec4e115c
oliveirazpm11-jpg <oliveirazpm11@gmail.com>
2026-05-30 09:29:26 -0300

    Add files via upload

---
+++ 05_loop_FOR/atividade02_loop_for.py
@@ -0,0 +1,7 @@
+#|---AUTOR:PIETRO OLIVEIRA---|
+#|---TABUADA COM LOOP FOR---|
+numero=2
+
+#LOOP FOR
+for i in range (1,101):
+    print(f"{numero} x {i} = {i * numero}")
---
+++ 05_loop_FOR/atividade03_loop_for.py
@@ -0,0 +1,5 @@
+#|---AUTOR:PIETRO OLIVEIRA---|
+#VARIAVEIS
+numero=int(input("digite o número: "))
+for i in range(1,11):
+    print(f"{numero} x {i} = {numero * i}")
